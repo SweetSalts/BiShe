b25550f9521b7b68dc58685df842920c9093b9c8
yansiwei <ysiwei97@163.com>
2018-03-05 14:43:45 +0800

    修改了添加实验站的接口，添加了经纬度两个输入框。

--- app/src/main/java/me/itangqi/buildingblocks/ui/fragment/BaseFragment.java
+++ app/src/main/java/me/itangqi/buildingblocks/ui/fragment/BaseFragment.java
@@ -1,7 +1,9 @@
 package me.itangqi.buildingblocks.ui.fragment;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -9,15 +11,22 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapPoi;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.Marker;
@@ -32,6 +41,7 @@ import java.util.List;
 import me.itangqi.buildingblocks.R;
 import me.itangqi.buildingblocks.utils.base.BaseInfo;
 import me.itangqi.buildingblocks.utils.base.DBHelper;
+import me.itangqi.buildingblocks.utils.base.ValueUtils;
 
 /**
  * Created by SweetSalt on 2018/3/1.
@@ -46,6 +56,8 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
     //控件
     private TextureMapView mMapView;
     private BaiduMap mBaiduMap;
+    private Dialog addDialog, delDialog;
+    EditText baseName, baseName1, basePosition, baseCode, baseLatitude, baseLongitude;
     //覆盖物相关
     private BitmapDescriptor mMaker;
     private RelativeLayout mMakerLy;
@@ -57,21 +69,21 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
 
         //申请权限
         List<String> permissionList = new ArrayList<>();
-        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission
-                .ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission
+                .ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
-        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.
-                READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.
+                READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(Manifest.permission.READ_PHONE_STATE);
         }
 
-        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.
-                WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.
+                WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
-        if(!permissionList.isEmpty()){
+        if (!permissionList.isEmpty()) {
             String[] permissions = permissionList.toArray(new String[permissionList.size()]);
             ActivityCompat.requestPermissions(getActivity(), permissions, 1);
         }
@@ -79,17 +91,17 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
-                if(grantResults.length > 0){
-                    for(int result : grantResults){
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
                         if (result != PackageManager.PERMISSION_GRANTED) {
                             Toast.makeText(getActivity(), "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
                             getActivity().finish();
                             return;
                         }
                     }
-                }else {
+                } else {
                     Toast.makeText(getActivity(), "发生未知错误", Toast.LENGTH_SHORT).show();
                     getActivity().finish();
                 }
@@ -106,6 +118,44 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
         initView();
         //初始化实验站位置
         initMaker();
+        //监听点击事件
+        mBaiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Bundle extraInfo = marker.getExtraInfo();
+                BaseInfo info = (BaseInfo) extraInfo.getSerializable("info");
+                ImageView iv = (ImageView) mMakerLy.findViewById(R.id.id_info_img);
+                TextView name = (TextView) mMakerLy.findViewById(R.id.id_info_name);
+
+                iv.setImageResource(R.mipmap.nanxiao);
+                name.setText(info.getBaseName());
+
+                final InfoWindow infoWindow;
+                TextView tv = new TextView(getActivity());
+                tv.setBackgroundResource(R.mipmap.popup);
+                tv.setPadding(30, 20, 30, 50);
+                tv.setText(info.getBaseName());
+
+                final LatLng latlng = marker.getPosition();
+                infoWindow = new InfoWindow(tv, latlng, -47);
+
+                mBaiduMap.showInfoWindow(infoWindow);
+                mMakerLy.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
+        mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMakerLy.setVisibility(View.GONE);
+                mBaiduMap.hideInfoWindow();
+            }
+
+            @Override
+            public boolean onMapPoiClick(MapPoi mapPoi) {
+                return false;
+            }
+        });
         return v;
     }
 
@@ -115,7 +165,7 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
     }
 
     private void initView() {
-        mMapView = (TextureMapView ) v.findViewById(R.id.bmapView);
+        mMapView = (TextureMapView) v.findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
         MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
         mBaiduMap.setMapStatus(msu);
@@ -147,7 +197,7 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
         LatLng latlng = null;
         Marker marker = null;
         OverlayOptions options;
-        for(BaseInfo info : infos){
+        for (BaseInfo info : infos) {
             //经纬度
             latlng = new LatLng(info.getLatitude(), info.getLongitude());
             //图标
@@ -157,7 +207,7 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
                     .zIndex(5);
             marker = (Marker) mBaiduMap.addOverlay(options);
             Bundle arg = new Bundle();
-            arg.putSerializable("info",info);
+            arg.putSerializable("info", info);
             marker.setExtraInfo(arg);
         }
         MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latlng);
@@ -165,7 +215,156 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case R.id.action_addBase:
+                addBaseDialog();// 添加基地的数据到数据库中
+                break;
+            case R.id.action_deleteBase:
+                deleteBaseDialog();// 删除基地数据
+                break;
+            case android.R.id.home:// 点击返回图标事件
+                getActivity().finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void addBaseDialog() {
+        // TODO Auto-generated method stub
+        // 填写所需数据的dialog，最后插入数据库
+        addDialog = new Dialog(getActivity());
+        addDialog.setTitle("填入详细数据");
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_addbase,
+                null);
+        baseName = (EditText) view.findViewById(R.id.et_baseName);
+        basePosition = (EditText) view.findViewById(R.id.et_basePosition);
+        baseCode = (EditText) view.findViewById(R.id.et_baseCode);
+        baseLatitude = (EditText) view.findViewById(R.id.et_baseLatitude);
+        baseLongitude = (EditText) view.findViewById(R.id.et_baseLongitude);
+        Button addbutton = (Button) view.findViewById(R.id.bt_addbase);
+        Button addbuttoncancel = (Button) view
+                .findViewById(R.id.bt_addbasecancle);
+        addbutton.setOnClickListener(this);
+        addbuttoncancel.setOnClickListener(this);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        addDialog.addContentView(view, layoutParams);
+        addDialog.show();
+    }
+
+    private void deleteBaseDialog() {
+        // TODO Auto-generated method stub
+        // 填入基地名称，如果数据库中存在该条数据，删除
+        delDialog = new Dialog(getActivity());
+        delDialog.setTitle("填入要删除的示范站名称");
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_delbase,
+                null);
+        baseName1 = (EditText) view.findViewById(R.id.et_baseName1);
+        Button addbutton1 = (Button) view.findViewById(R.id.bt_delbase);
+        Button addbuttoncancel1 = (Button) view
+                .findViewById(R.id.bt_delbasecancle);
+        addbutton1.setOnClickListener(this);
+        addbuttoncancel1.setOnClickListener(this);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        delDialog.addContentView(view, layoutParams);
+        delDialog.show();
+    }
+
+    public static boolean isEmpty(EditText et1, EditText et2, EditText et3) {
+        String str1 = et1.getText().toString().trim();
+        String str2 = et2.getText().toString().trim();
+        String str3 = et3.getText().toString().trim();
+        if (ValueUtils.isStrEmpty(str1)) {
+            return false;
+        } else if (ValueUtils.isStrEmpty(str2)) {
+            return false;
+        } else if (ValueUtils.isStrEmpty(str3)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean isEmpty(EditText et1) {
+        String str1 = et1.getText().toString().trim();
+        if (ValueUtils.isStrEmpty(str1)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onClick(View arg0) {
+        switch (arg0.getId()) {
+            case R.id.bt_addbase:
+                if (isEmpty(baseName, basePosition, baseCode)) {
+                    if (dbHelper == null) {
+                        dbHelper = new DBHelper(getActivity());
+                    }
+                    ContentValues valu = new ContentValues();
+                    valu.putNull("_baseid");
+                    valu.put("basename", baseName.getText().toString());
+                    valu.put("baseposition", basePosition.getText().toString());
+                    valu.put("basecode", baseCode.getText().toString());
+                    valu.put("latitude", baseLatitude.getText().toString());
+                    valu.put("longitude", baseLongitude.getText().toString());
+                    dbHelper.insert("baseInfoTb", valu);
+                    addDialog.dismiss();
+                    showBase();
+                } else {
+                    Toast.makeText(getActivity(), "你还有数据没有填写！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.bt_addbasecancle:
+                addDialog.dismiss();
+                break;
+            case R.id.bt_delbase:// 删除示范站
+                if (isEmpty(baseName1)) {
+                    if (dbHelper == null) {
+                        dbHelper = new DBHelper(getActivity());
+                    }
+                    String basecode = dbHelper.queryBaseCode(baseName1.getText()
+                            .toString().trim());
+                    dbHelper.relationdel(basecode);// 先删除该示范站下的相机
+                    dbHelper.del("baseInfoTb", baseName1.getText().toString()
+                            .trim());// 删除示范站
+                    delDialog.dismiss();
+                    showBase();
+                    Toast.makeText(getActivity(), "已删除！",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "你还有数据没有填写！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.bt_delbasecancle:
+                delDialog.dismiss();
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
